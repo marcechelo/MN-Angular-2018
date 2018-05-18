ce72d05941c1b28a9ed1703a02b945fc2f2463d8
intiinside <inti.poaquiza@epn.edu.ec>
2018-05-18 17:44:03 -0500

    clase 18/05/2018

--- spoiler-app-v6/src/app/app.component.ts
+++ spoiler-app-v6/src/app/app.component.ts
@@ -1,33 +1,57 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',//Selector o etiquta
-  templateUrl: './app.component.html', //Estructura
-  styleUrls: ['./app.component.css']//Estilos
+  selector: 'app-root', // Selector o etiqueta
+  templateUrl: './app.component.html', // Estructura
+  styleUrls: ['./app.component.css'] // Estilos
 })
+
 export class AppComponent {
-  // title = 'app';
-  // titulo= 'Hola Amigos';
-  // nombre = 'marcelo';
-  // apellido = 'nieto';
-  // edad = 23;
-  // sueldo = 1;
-  // fechaNacimeinto = new Date('1995/05/06');
-  // nombreBoton = 'Cambiar Clase';
+
+  mostrarHola = true;
+  arregloNumeros = [1, 2, 3, 4, 5, 6];
+
+  arregloPeliculas = [
+    {
+      nombre: 'Tomb Raider',
+      descripcion: 'Alicia Vikander, Walton Goggins',
+      esEstreno: false,
+      urlImagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg'
+    },
+    {
+      nombre: 'Ready Player One',
+      descripcion: 'Tye Sheridan, Olivia Cooke',
+      esEstreno: false,
+      urlImagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzFQprASMSiRnRHYgQl5sHoGr8GiMI9IB2XVIQP_tmD61KFgUY'
+    },
+  ];
+
+
+  alertar(evento) {
+    console.log('Evento: ', evento); // true
+    alert('DIO CLICK EN ES ESTRENO!!!!!!!!!');
+  }
+
+
+  // nombre = 'Adrian';
+  // apellido = 'Eguez';
+  // edad = 28;
+  // sueldo = 3411234.13;
+  // fechaNacimiento = new Date('1989-06-10');
+  // nombreBoton = 'Cambiar color';
   // nombreClase = 'sa-titulo';
   // clase = 'papa';
   //
-  // cambiarClaseDeTitulo (){
-  //   const nombreClaseEsTitulo = this.nombreClase ==='sa-titulo';
-  //   if(nombreClaseEsTitulo){
+  // cambiarClaseDeTitulo() {
+  //   const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+  //   if (nombreClaseEsTitulo) {
   //     this.nombreClase = 'sa-subtitulo';
-  //   }else{
+  //   } else {
   //     this.nombreClase = 'sa-titulo';
   //   }
   // }
   //
-  // aplicarClase(valorInput: string){
+  // aplicarClase(valorInput: string) {
   //   this.nombreClase = valorInput;
-  //   this.titulo = valorInput;
   // }
 }
--- spoiler-app-v6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ spoiler-app-v6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -1,27 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-pelicula-banner',
   templateUrl: './pelicula-banner.component.html',
   styleUrls: ['./pelicula-banner.component.css']
 })
-export class PeliculaBannerComponent implements OnInit {
+export class PeliculaBannerComponent implements OnInit, OnChanges {
 
-  urlImagen = 'https://www.hdwallpapers.in/download/alicia_vikander_as_lara_croft_in_tomb_raider-wide.jpg';
-  descripcionImagen = 'Imagen de la pelicula Tomb Raider';
-  nombrePelicula = 'Tomb Raider';
-  descripcionPelicula = 'Alicia vikander,Walton Googins';
-  esEstreno = true;
+  @Input() urlImagen: string;
+  @Input() descripcionImagen: string;
+  @Input() nombrePelicula: string;
+  @Input() descripcionPelicula: string;
+  @Input() esEstreno: boolean;
+
+  @Output() dioClickEnEstado: EventEmitter<boolean> = new EventEmitter();
+
+
   textoEstreno: string;
   claseEstreno: string;
-  claseAlerta: String;
-  tipoalerta: ['primary','secondary','success'];
 
   constructor() {
+    // undefined
   }
 
   ngOnInit() {
+    console.log("Inicio y seteo color", this.nombrePelicula);
+    this.setearColorYClase();
+  }
 
+  ngOnChanges(propiedadesActualizadas) {
+    console.log('Algo', propiedadesActualizadas);
+    if (propiedadesActualizadas.esEstreno) {
+      this.setearColorYClase();
+    }
+  }
+
+  setearColorYClase() {
     if (this.esEstreno) {
       this.textoEstreno = 'Estreno';
       this.claseEstreno = 'sa-color-estado-rosado';
@@ -29,11 +43,10 @@ export class PeliculaBannerComponent implements OnInit {
       this.textoEstreno = 'Proximamente';
       this.claseEstreno = 'sa-color-estado-amarillo';
     }
+  }
 
-
-
-
-
+  hizoClickEnEstado() {
+    this.dioClickEnEstado.emit(true);
   }
 
 }
